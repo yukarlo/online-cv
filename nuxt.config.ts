cf1119ff88eaae0e0128052bafedf0dbfe943a0e
Karlo <karlo.miguel.p.yu@gmail.com>
2025-06-01 00:56:52 +0200

    migrate to pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@nuxt/test-utils", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxt/ui", "@nuxt/eslint"],
+	modules: [
+		"@nuxt/eslint",
+		"@nuxt/fonts",
+		"@nuxt/icon",
+		"@nuxt/image",
+		"@nuxt/ui",
+		"@nuxt/test-utils",
+		"@nuxt/scripts",
+	],
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css"],
 	compatibilityDate: "2025-05-15",
